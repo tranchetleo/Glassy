2a13fc43d6ec7fb8c22329a760b538b9a55e602f
absolute leo <tranchetleo@gmail.com>
2024-12-24 16:57:09 +0100

    Ajout de la fonction ScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import './assets/glassy.css';
+import ScrollToTop from './components/ScrollToTop';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
@@ -10,6 +11,7 @@ import Produits from './pages/Produits';
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path='/'
